6c83837da57930bcfd7584e3ee14f7e40f1aafb6
Joshua Stauter <jstauter@jungroup.com>
2016-07-28 18:04:22 -0400

    torquebox on port 8080

---
+++ Gemfile
@@ -0,0 +1,4 @@
+source 'https://rubygems.org'
+
+gem 'rails', '4.2.7'
+gem 'torquebox-web', '4.0.0.beta3'
---
+++ config/boot.rb
@@ -0,0 +1,22 @@
+ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../../Gemfile', __FILE__)
+
+require 'bundler/setup' # Set up gems listed in the Gemfile.
+
+require 'rails/commands/server'
+
+module Rails
+    class Server
+        alias :original_default_options :default_options
+        def default_options
+            original_default_options.merge Host: ENV['IP'], Port: 8080
+        end
+    end
+end
+
+module Rack
+    module Handler
+        def self.default options = {}
+            pick ['torquebox']
+        end
+    end
+end
