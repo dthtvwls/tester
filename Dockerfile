858aa7232a1176fb216f83f317387426da7824fc
Joshua Stauter <jstauter@jungroup.com>
2016-07-29 00:21:47 -0400

    organize

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,6 @@
 FROM jruby:1.7.25-jre
-COPY . .
+COPY Gemfile*
 RUN bundle
+COPY . .
 EXPOSE 8080
 CMD rails server
--- config/boot.rb
+++ config/boot.rb
@@ -8,7 +8,10 @@ module Rails
     class Server
         alias :original_default_options :default_options
         def default_options
-            original_default_options.merge Port: 8080 # , Host: ENV['IP']
+            original_default_options.merge(
+                Host: ENV['HOST'] || UDPSocket.open { |socket| socket.connect('192.168.96.1', 1); socket.addr.last },
+                Port: 8080
+            )
         end
     end
 end
